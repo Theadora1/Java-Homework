6abcbf72497f6131547290984403696182950d32
Theadora Gerber <theadora.gerber@hhcc.com>
2014-09-25 15:28:34 -0400

    Java-Homework

---
+++ java.js
@@ -0,0 +1,79 @@
+
+// this is a variable
+var x = 2
+var b = 4
+// variables can do basic math with numvers or pulling from varaiables (above)
+2 + 2
+x + b
+// this is an alert
+alert ('Hi there!');
+/* this
+is
+console */
+console.log ("This is java fun");
+/* these are data types
+string: "My name is Thea"
+number (floats): "2, 2.2, 8000";
+boolean: true, false
+undefined: (no value, var with no value);
+*/
+// this is an array
+["car", "trucks", "cats", "dogs", "18", "32"];
+// this is a variable class array
+var colors = ["pink", "mustard", "green", "red"];
+// this is using an array index
+var colors = ["pink", "mustard", "green", "red"];
+console.log (colors[0]);
+// this is a multidimensional array
+var = ["blue", "green", "pink"];
+var = ["five", "eight", "one hundred"];
+var = ["car", "trucks", "cats", "dogs", "18", "32"];
+// Arrayception: an array can store other arrays
+var states = ["michigan", "new york", "florida"];
+var countires = ["US", "France", "Turkey"];
+var towns = ["Saratoga", "Hackensack", "Albany", "Ossining"];
+console.log (towns [3][1]);
+//testing
+"Kitty1" === "Kitty2"
+(5+2) == 7
+"Kitty1" !== "Kitty2";
+50 > 30
+// Logic: Else If
+If(x<600) {
+	console.log("x is less than 600");
+} else {
+	console.log("x is greater than 600");
+}
+// Logic: Else If.. Then
+If(14>3) {
+	console.log("14 is greater than 3");
+} else if(14===14);
+console.log("14 really equals 14");
+} else {
+	console.log("14 is less than 3");
+}
+// Function
+function subtract3(somenumber) {
+	return somenumber - 3;
+}
+	console.log(subtract3(5));
+
+function alertname(name) {
+	return alert (name);
+}
+alertaddy("Bill");
+
+var tv1 = "Game of Thrones"
+var tv2 = "Orphan Black"
+var tv3 = "The Strain"
+
+function tvchoice(tv) {
+	if (tv === tv1) {
+		alert ("You will watch Game of Thrones");
+	}
+	else if (tv === tv2) {
+		alert ("You will watch Orphan Black");
+	}
+	else if (tv === tv3) {
+		alert ("You will watch The Strain");
+	}
